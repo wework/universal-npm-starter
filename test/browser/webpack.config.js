8b3e94ffb30f6df19f65f03f6751fe08adcac91d
mattjstar <mattjstar@gmail.com>
2017-04-08 16:02:45 -0400

    upgrade to webpack 2

--- test/browser/webpack.config.js
+++ test/browser/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     noParse: [
       /node_modules(\\|\/)sinon/
     ],
-    loaders: [
+    rules: [
       {
         test: /\.json$/,
         loader: 'json-loader'
@@ -21,7 +21,7 @@ module.exports = {
       },
       {
         test: /sinon(\\|\/)pkg(\\|\/)sinon\.js/,
-        loader: 'imports?define=>false,require=>false'
+        loader: 'imports-loader?define=>false,require=>false'
       }
     ]
   },
